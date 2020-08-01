694bc5bd71f1f8981c8ecd20c6ee84bf2163f8e3
Richard Moss <richard.moss@cyotek.com>
2020-08-01 08:19:23 +0100

    Reinstated partial assembly info from before SDK switch
    * Re-introduced the same ComVisible, CLSCompliant, Guid and AssemblyVersion as previous builds
    * Disabled automatic generation of AssemblyVersion info to avoid the newtonsoft plague
    * Reinstated complete AssemblyInfo.cs for test lib

---
+++ Cyotek.Collections.Generic.CircularBuffer.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cyotek.Collections.Generic.CircularBuffer (Tests)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cyotek.Collections.Generic.CircularBuffer")]
+[assembly: AssemblyCopyright("Copyright © 2014-2020 Cyotek Ltd. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("c82afd42-a3ab-490a-8719-242299d75b01")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
---
+++ Cyotek.Collections.Generic.CircularBuffer/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("1a902c02-561d-48c4-9fe3-0c3c707589d7")]
+[assembly: AssemblyVersion("1.0.0.0")]
