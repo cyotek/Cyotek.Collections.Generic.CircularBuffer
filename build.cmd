8ecaef7cd15567d0f8ec7905ccd6e71c0e9ae8c6
Richard Moss <richard.moss@cyotek.com>
2020-08-01 08:37:58 +0100

    Tidying up build.cmd
    Also now deletes old package artifacts to make it easier to push

--- build.cmd
+++ build.cmd
@@ -4,30 +4,23 @@ SETLOCAL
 
 CALL ..\..\..\build\set35vars.bat
 
-REM %msbuildexe% Cyotek.Collections.Generic.CircularBuffer.sln /p:Configuration=Release /verbosity:minimal /nologo /t:Clean,Build
-REM CALL signcmd Cyotek.Collections.Generic.CircularBuffer\bin\Release\Cyotek.Collections.Generic.CircularBuffer.dll
+SET RELDIR=Cyotek.Collections.Generic.CircularBuffer\bin\Release\
+SET PRJFILE=Cyotek.Collections.Generic.CircularBuffer\Cyotek.Collections.Generic.CircularBuffer.csproj
 
-dotnet build Cyotek.Collections.Generic.CircularBuffer\Cyotek.Collections.Generic.CircularBuffer.csproj --configuration Release
-CALL signcmd Cyotek.Collections.Generic.CircularBuffer\bin\Release\net40\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd Cyotek.Collections.Generic.CircularBuffer\bin\Release\net452\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd Cyotek.Collections.Generic.CircularBuffer\bin\Release\net462\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd Cyotek.Collections.Generic.CircularBuffer\bin\Release\net472\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd Cyotek.Collections.Generic.CircularBuffer\bin\Release\net48\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd Cyotek.Collections.Generic.CircularBuffer\bin\Release\netcoreapp2.1\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd Cyotek.Collections.Generic.CircularBuffer\bin\Release\netcoreapp2.2\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd Cyotek.Collections.Generic.CircularBuffer\bin\Release\netcoreapp3.1\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd Cyotek.Collections.Generic.CircularBuffer\bin\Release\netstandard2.0\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd Cyotek.Collections.Generic.CircularBuffer\bin\Release\netstandard2.1\Cyotek.Collections.Generic.CircularBuffer.dll
-dotnet pack Cyotek.Collections.Generic.CircularBuffer/Cyotek.Collections.Generic.CircularBuffer.csproj --configuration Release --no-build
+IF EXIST %RELDIR%*.nupkg DEL /F %RELDIR%*.nupkg
+IF EXIST %RELDIR%*.snupkg DEL /F %RELDIR%*.snupkg
 
-REM PUSHD
-
-REM CD releases
-
-REM NUGET pack ..\Cyotek.Collections.Generic.CircularBuffer\Cyotek.Collections.Generic.CircularBuffer.csproj -Prop Configuration=Release
-
-REM %zipexe% a -bd -tZip Cyotek.Collections.Generic.CircularBuffer.x.x.x.x.zip ..\Cyotek.Collections.Generic.CircularBuffer\bin\Release\Cyotek.Collections.Generic.CircularBuffer.dll
-
-REM POPD
+dotnet build %PRJFILE% --configuration Release
+CALL signcmd %RELDIR%net40\Cyotek.Collections.Generic.CircularBuffer.dll
+CALL signcmd %RELDIR%net452\Cyotek.Collections.Generic.CircularBuffer.dll
+CALL signcmd %RELDIR%net462\Cyotek.Collections.Generic.CircularBuffer.dll
+CALL signcmd %RELDIR%net472\Cyotek.Collections.Generic.CircularBuffer.dll
+CALL signcmd %RELDIR%net48\Cyotek.Collections.Generic.CircularBuffer.dll
+CALL signcmd %RELDIR%netcoreapp2.1\Cyotek.Collections.Generic.CircularBuffer.dll
+CALL signcmd %RELDIR%netcoreapp2.2\Cyotek.Collections.Generic.CircularBuffer.dll
+CALL signcmd %RELDIR%netcoreapp3.1\Cyotek.Collections.Generic.CircularBuffer.dll
+CALL signcmd %RELDIR%netstandard2.0\Cyotek.Collections.Generic.CircularBuffer.dll
+CALL signcmd %RELDIR%netstandard2.1\Cyotek.Collections.Generic.CircularBuffer.dll
+dotnet pack %PRJFILE% --configuration Release --no-build
 
 ENDLOCAL
