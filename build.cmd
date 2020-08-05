d77ab01bb59931b88c6b0d8710ed79c4b68ac173
Richard Moss <richard.moss@cyotek.com>
2020-08-05 17:09:49 +0100

    Renamed obnoxiously long directories

--- build.cmd
+++ build.cmd
@@ -4,8 +4,8 @@ SETLOCAL
 
 CALL ..\..\..\build\set35vars.bat
 
-SET RELDIR=Cyotek.Collections.Generic.CircularBuffer\bin\Release\
-SET PRJFILE=Cyotek.Collections.Generic.CircularBuffer\Cyotek.Collections.Generic.CircularBuffer.csproj
+SET RELDIR=src\bin\Release\
+SET PRJFILE=src\Cyotek.Collections.Generic.CircularBuffer.csproj
 
 IF EXIST %RELDIR%*.nupkg DEL /F %RELDIR%*.nupkg
 IF EXIST %RELDIR%*.snupkg DEL /F %RELDIR%*.snupkg
