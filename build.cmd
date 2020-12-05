d56ae31707a28d6b0866c45a29f427f2116a97be
Richard Moss <richard.moss@cyotek.com>
2020-12-05 09:49:27 +0000

    Added global.json
    Can't build .NET Framework 3.5 with 5.0 SDK, forcing temporary drop to 3.1

--- build.cmd
+++ build.cmd
@@ -4,25 +4,30 @@ SETLOCAL
 
 CALL ..\..\..\build\set35vars.bat
 
+SET BASENAME=Cyotek.Collections.Generic.CircularBuffer
 SET RELDIR=src\bin\Release\
-SET PRJFILE=src\Cyotek.Collections.Generic.CircularBuffer.csproj
+SET PRJFILE=src\%BASENAME%.csproj
+SET DLLNAME=%BASENAME%.dll
 
 IF EXIST %RELDIR%*.nupkg DEL /F %RELDIR%*.nupkg
 IF EXIST %RELDIR%*.snupkg DEL /F %RELDIR%*.snupkg
 
 dotnet build %PRJFILE% --configuration Release
-CALL signcmd %RELDIR%net35\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd %RELDIR%net40\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd %RELDIR%net452\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd %RELDIR%net462\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd %RELDIR%net472\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd %RELDIR%net48\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd %RELDIR%netcoreapp2.1\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd %RELDIR%netcoreapp2.2\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd %RELDIR%netcoreapp3.1\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd %RELDIR%netstandard2.0\Cyotek.Collections.Generic.CircularBuffer.dll
-CALL signcmd %RELDIR%netstandard2.1\Cyotek.Collections.Generic.CircularBuffer.dll
+
+CALL signcmd %RELDIR%net35\%DLLNAME%
+CALL signcmd %RELDIR%net40\%DLLNAME%
+CALL signcmd %RELDIR%net452\%DLLNAME%
+CALL signcmd %RELDIR%net462\%DLLNAME%
+CALL signcmd %RELDIR%net472\%DLLNAME%
+CALL signcmd %RELDIR%net48\%DLLNAME%
+CALL signcmd %RELDIR%netcoreapp2.1\%DLLNAME%
+CALL signcmd %RELDIR%netcoreapp2.2\%DLLNAME%
+CALL signcmd %RELDIR%netcoreapp3.1\%DLLNAME%
+CALL signcmd %RELDIR%netstandard2.0\%DLLNAME%
+CALL signcmd %RELDIR%netstandard2.1\%DLLNAME%
+
 dotnet pack %PRJFILE% --configuration Release --no-build
+
 CALL sign-package %RELDIR%*.nupkg
 CALL sign-package %RELDIR%*.snupkg
 
