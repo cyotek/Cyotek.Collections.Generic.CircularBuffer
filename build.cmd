1ba033cbe1913c808fbd6b187746e584bdb2a18e
Richard Moss <richard.moss@cyotek.com>
2025-08-30 08:44:53 +0100

    DEL: Drop obsolete targets

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@
 
 SETLOCAL
 
-CALL ..\..\..\build\set35vars.bat
+CALL %CTKBLDROOT%\setupEnv.cmd
 
 SET BASENAME=Cyotek.Collections.Generic.CircularBuffer
 SET RELDIR=src\bin\Release\
@@ -14,17 +14,9 @@ IF EXIST %RELDIR%*.snupkg DEL /F %RELDIR%*.snupkg
 
 dotnet build %PRJFILE% --configuration Release
 
-CALL signcmd %RELDIR%net35\%DLLNAME%
-CALL signcmd %RELDIR%net40\%DLLNAME%
-CALL signcmd %RELDIR%net452\%DLLNAME%
 CALL signcmd %RELDIR%net462\%DLLNAME%
-CALL signcmd %RELDIR%net472\%DLLNAME%
-CALL signcmd %RELDIR%net48\%DLLNAME%
-CALL signcmd %RELDIR%netcoreapp2.1\%DLLNAME%
-CALL signcmd %RELDIR%netcoreapp2.2\%DLLNAME%
-CALL signcmd %RELDIR%netcoreapp3.1\%DLLNAME%
+CALL signcmd %RELDIR%net8.0\%DLLNAME%
 CALL signcmd %RELDIR%netstandard2.0\%DLLNAME%
-CALL signcmd %RELDIR%netstandard2.1\%DLLNAME%
 
 dotnet pack %PRJFILE% --configuration Release --no-build
 
