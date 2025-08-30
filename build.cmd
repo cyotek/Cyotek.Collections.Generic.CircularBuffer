233bdc3edc246c670f5d9f073f6121c93a4c6111
Richard Moss <richard.moss@cyotek.com>
2025-08-30 08:47:19 +0100

    REF: Update test packages

--- build.cmd
+++ build.cmd
@@ -9,6 +9,9 @@ SET RELDIR=src\bin\Release\
 SET PRJFILE=src\%BASENAME%.csproj
 SET DLLNAME=%BASENAME%.dll
 
+dotnet test --configuration Release
+IF %ERRORLEVEL% NEQ 0 GOTO :testsfailed
+
 IF EXIST %RELDIR%*.nupkg DEL /F %RELDIR%*.nupkg
 IF EXIST %RELDIR%*.snupkg DEL /F %RELDIR%*.snupkg
 
@@ -24,3 +27,8 @@ CALL sign-package %RELDIR%*.nupkg
 CALL sign-package %RELDIR%*.snupkg
 
 ENDLOCAL
+
+GOTO :eof
+
+:testsfailed
+CECHO {0c}ERROR: *** TEST RUN FAILED ***{#}{\n}
